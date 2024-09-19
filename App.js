31db90c6e3c05be0744eb18748766f9aa1c6811b
Rezmii <bartoszrezmer20@gmail.com>
2024-09-19 20:49:55 +0200

    Basic layout and navigation

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Provider as PaperProvider, Appbar } from "react-native-paper";
+import { Text, View, StyleSheet } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Ekran Główny</Text>
+    </View>
+  );
+}
+
+function GoalsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Twoje Cele</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Ustawienia</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Appbar.Header>
+          <Appbar.Content title="Goal Tracker" />
+        </Appbar.Header>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+              if (route.name === "Home") {
+                iconName = "home";
+              } else if (route.name === "Goals") {
+                iconName = "list";
+              } else if (route.name === "Settings") {
+                iconName = "settings";
+              }
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            headerShown: false,
+          })}
+          tabBarOptions={{
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray",
+          }}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Goals" component={GoalsScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
   },
 });
